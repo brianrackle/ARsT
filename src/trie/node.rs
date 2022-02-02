8a47036aadf54984e98f4af2767ea371c864a64b
brianrackle <brianrackle@hotmail.com>
2022-02-01 17:58:51 -0800

    trying to solve borrow mut problem with add then upgrade... not going well

--- src/trie/node.rs
+++ src/trie/node.rs
@@ -7,17 +7,10 @@ use std::fmt::{Debug, Formatter};
 type Link = Option<Box<dyn Node>>;
 // type LinkMutRef = Option<&mut Box<dyn Node>>;
 
-
+//TODO explore recursive solution like add_multiple(remaining_value).add_single(current_value) would require owning
 pub trait Node : Debug {
-    // fn add(&mut self, value: &[u8]) -> N {
-    //     match value {
-    //         [] => self.add_empty_case(),
-    //         [cur_value] => self.add_single_case(cur_value),
-    //         [cur_value, remaining_values @ ..] => {
-    //             self.add_multiple_case(cur_value, remaining_values)
-    //         }
-    //     }
-    // }
+    fn get_location(&self, cur_value :&u8) -> Option<usize>;
+    fn insert(&mut self, cur_value: &u8, location :Option<usize>) -> Option<&mut Box<dyn Node>>;
     fn add(&mut self, cur_value: &u8) -> Option<&mut Box<dyn Node>>;
     fn upgrade(&mut self) -> Box<dyn Node>; //FIXME would be best if upgrade moved self look into using another trait see NodeUpgrade
     fn terminate(&mut self);
@@ -78,30 +71,46 @@ impl Node4 {
     }
 }
 
+//series a equity
+//$170
+//15%
+//likes work wouldnt change
+//15-16
+
 impl Node for Node4 {
-    // adds a single value and returns the new current if its upgraded, and the node where the value was added
-    fn add(&mut self, cur_value: &u8) -> Option<&mut Box<dyn Node>> {
-        //check if value exists already
-        if let Some(index) = self
+    fn get_location(&self, cur_value: &u8) -> Option<usize> {
+       if let Some(index) = self
             .keys
             .iter()
-            .position(|v| v.is_some() && v.unwrap() == *cur_value)
-        {
-            self.children[index].as_mut()
-        } else {
-            //value doesnt exist yet
-            //expand to node16 and then add new value
-            if !self.is_full() {
-                //add value to existing Node4 if there is room
-                let target_index = self.size;
-                self.keys[target_index] = Some(*cur_value);
-                self.children[target_index] = Some(Box::new(Node4::new()));
-                self.size += 1;
-                self.children[target_index].as_mut()
-            } else {
-                None
-            }
+            .position(|v| if let Some(cmp) = v { cmp == cur_value } else { false }) {
+           Some(index) //FIXME might be able to do a comparison in add so that Exists and Insert arent both needed
+       } else if !self.is_full() {
+           Some(self.size)
+       } else {
+           None
+       }
+    }
+
+    // adds a single value and returns the new current if its upgraded, and the node where the value was added
+    fn insert(&mut self, cur_value: &u8, location :Option<usize>) -> Option<&mut Box<dyn Node>> {
+        match location {
+            Some(index) => {
+                if let Some(_) = self.keys[index] {
+                    self.children[index].as_mut()
+                } else {
+                    self.keys[index] = Some(*cur_value);
+                    self.children[index] = Some(Box::new(Node4::new()));
+                    self.size += 1;
+                    self.children[index].as_mut()
+                }
+            },
+            None => None,
         }
+    }
+
+    // adds a single value and returns the next node if add succeeded
+    fn add(&mut self, cur_value: &u8) -> Option<&mut Box<dyn Node>> {
+        self.insert(cur_value, self.get_location(cur_value))
     }
 
     fn upgrade(&mut self) -> Box<dyn Node> {
@@ -184,20 +193,26 @@ impl Node16 {
 }
 
 impl Node for Node16 {
-    fn add(&mut self, cur_value: &u8) -> Option<&mut Box<dyn Node>> {
+    fn get_location(&self, cur_value: &u8) -> Option<usize> {
         //check if value exists already
         match self
             .keys
-            .binary_search_by(|probe| Node16::val_cmp(probe, &Some(*cur_value)))
-        {
+            .binary_search_by(|probe| Node16::val_cmp(probe, &Some(*cur_value))) {
             Ok(index) => {
-                //FIXME can do None/Some check for extra error checking
-                self.children[index].as_mut()
+                Some(index)
             }
             Err(index) => {
-                //expand to node48 and then add new value
-                if !self.is_full() {
-                    //add value in sorted order to existing Node16 if there is room
+                if !self.is_full() { Some(index) } else { None }
+            }
+        }
+    }
+
+    fn insert(&mut self, cur_value: &u8, location: Option<usize>) -> Option<&mut Box<dyn Node>> {
+        match location {
+            Some(index) => {
+                if let Some(_) = self.keys[index] {
+                    self.children[index].as_mut()
+                } else {
                     self.keys[index..].rotate_right(1); //shift right from index
                     self.keys[index] = Some(*cur_value);
 
@@ -206,11 +221,14 @@ impl Node for Node16 {
 
                     self.size += 1;
                     self.children[index].as_mut()
-                } else {
-                    None
                 }
-            }
+            },
+            None => None,
         }
+    }
+
+    fn add(&mut self, cur_value: &u8) -> Option<&mut Box<dyn Node>> {
+        self.insert(cur_value, self.get_location(cur_value))
     }
 
     fn upgrade(&mut self) -> Box<dyn Node> {
@@ -274,22 +292,36 @@ impl Node48 {
 
 
 impl Node for Node48 {
-    fn add(&mut self, cur_value: &u8) -> Option<&mut Box<dyn Node>> {
+    fn get_location(&self, cur_value: &u8) -> Option<usize> {
         let cur_value_index = *cur_value as usize;
-        //if exists
-        if let Some(key) = self.keys[cur_value_index] {
-            let key_index = key as usize;
-            self.children[key_index].as_mut()
+        if let Some(_) = self.keys[cur_value_index] {
+            Some(cur_value_index)
         } else if !self.is_full() {
-            //add to self
-            let target_index = self.size;
-            self.keys[cur_value_index] = Some(self.size as u8);
-            self.children[target_index] = Some(Box::new(Node4::new()));
-            self.size += 1;
-            self.children[target_index].as_mut()
+            Some(cur_value_index)
         } else {
             None
         }
+    }
+
+    fn insert(&mut self, cur_value: &u8, location: Option<usize>) -> Option<&mut Box<dyn Node>> {
+        match location {
+            Some(index) => {
+                if let Some(child_index) = self.keys[index] {
+                    self.children[child_index as usize].as_mut()
+                } else {
+                    let target_index = self.size;
+                    self.keys[index] = Some(target_index as u8);
+                    self.children[target_index] = Some(Box::new(Node4::new()));
+                    self.size += 1;
+                    self.children[target_index].as_mut()
+                }
+            },
+            None => None,
+        }
+    }
+
+    fn add(&mut self, cur_value: &u8) -> Option<&mut Box<dyn Node>> {
+        self.insert(cur_value, self.get_location(cur_value))
     }
 
     fn upgrade(&mut self) -> Box<dyn Node> {
@@ -352,19 +384,35 @@ impl Node256 {
 }
 
 impl Node for Node256 {
-    fn add(&mut self, cur_value: &u8) -> Option<&mut Box<dyn Node>> {
+    fn get_location(&self, cur_value: &u8) -> Option<usize> {
         let cur_value_index = *cur_value as usize;
-        //if exists
         match self.children[cur_value_index] {
-            None => {
-                self.children[cur_value_index] = Some(Box::new(Node4::new()));
-                self.size += 1;
-                self.children[cur_value_index].as_mut()
-            }
             Some(_) => {
-                self.children[cur_value_index].as_mut()
+                Some(cur_value_index)
+            }
+            None => {
+                Some(cur_value_index)
             }
         }
+    }
+
+    fn insert(&mut self, cur_value: &u8, location: Option<usize>) -> Option<&mut Box<dyn Node>> {
+        match location {
+            Some(index) => {
+                if self.children[index].is_some() {
+                    self.children[index].as_mut()
+                } else {
+                    self.children[index] = Some(Box::new(Node4::new()));
+                    self.size += 1;
+                    self.children[index].as_mut()
+                }
+            },
+            None=> None,
+        }
+    }
+
+    fn add(&mut self, cur_value: &u8) -> Option<&mut Box<dyn Node>> {
+        self.insert(cur_value, self.get_location(cur_value))
     }
 
     fn upgrade(&mut self) -> Box<dyn Node> {
@@ -397,53 +445,6 @@ impl Node for Node256 {
 
     fn as_any(&self) -> &dyn Any {
         self
-    }
-}
-
-//Old implementation starts here
-pub type OldLink = Option<Box<OldNode>>;
-pub struct OldNode {
-    children: [OldLink; 257],
-}
-
-impl OldNode {
-    pub fn new() -> Self {
-        OldNode {
-            children: arr![None; 257],
-        }
-    }
-
-    pub fn get_node(&self, i: usize) -> Option<&OldNode> {
-        self.children[i].as_ref().map(|c| c.as_ref())
-    }
-
-    pub fn add(&mut self, value: &[u8], match_type: &Match) {
-        match match_type {
-            Match::Exact | Match::Prefix => {
-                let mut cur = self;
-                for c in value {
-                    cur = cur.children[(*c) as usize].get_or_insert(Box::new(OldNode::new()));
-                }
-                //add terminal char when match is exact
-                if let Match::Exact = match_type {
-                    cur.children[257 - 1] = Some(Box::new(OldNode::new()))
-                }
-            }
-            Match::PrefixPostfix => {
-                //takes 0+n first characters off string
-                let mut cur: &mut OldNode;
-                for j in 0..value.len() {
-                    cur = self;
-                    for c in value[j..].iter() {
-                        cur = cur.children[(*c) as usize].get_or_insert(Box::new(OldNode::new()));
-                    }
-                }
-            }
-        }
-    }
-
-    pub fn exists(&self, c: u8) -> Option<&OldNode> {
-        self.children[c as usize].as_ref().map(|c| c.as_ref())
     }
 }
 
--- src/trie/tree.rs
+++ src/trie/tree.rs
@@ -1,11 +1,12 @@
 use crate::trie::enums::{Case, Match};
-use crate::trie::node::{OldLink, OldNode};
+use crate::trie::node::{Node, Node4};
 use std::borrow::Borrow;
+use std::mem;
 
 pub struct Trie {
     matching: Match,
     case: Case,
-    index: OldNode, //change this to Link for consistency
+    root: Box<dyn Node>, //change this to Link for consistency
 }
 
 //FIXME Match should be implemented at tree level
@@ -15,37 +16,106 @@ pub struct Trie {
 impl Trie {
     pub fn new(matching: Match, case: Case) -> Self {
         Trie {
-            matching: matching,
-            case: case,
-            index: OldNode::new(),
+            matching,
+            case,
+            root: Box::new(Node4::new())
         }
     }
 
-    pub fn add(&mut self, value: &str) {
-        if value.len() != 0 {
-            self.index.add(value.as_bytes(), &self.matching)
-        }
-    }
+    // //FIXME make iterative version l
+    // fn add_to(node :&mut Box<dyn Node> , value :&[u8]) -> Option<Box<dyn Node>> {
+    //     match value {
+    //         [] => {
+    //             node.terminate();
+    //             None
+    //         },
+    //         [cur_value] => {
+    //             if let Some(next_node) = node.add(cur_value) {
+    //                 Trie::add_to(next_node, &[]);
+    //                 Some(mem::replace(node, Box::new(Node4::new())))
+    //             } else {
+    //                 let mut node = node.upgrade();
+    //                 let next_node = node.add(cur_value).expect("Internal error: Upgrade occurred but add still not possible");
+    //                 Trie::add_to(next_node, &[]);
+    //                 Some(node)
+    //             }
+    //         },
+    //         [cur_value, remaining_values @ ..] => {
+    //             if let Some(next_node) = node.add(cur_value) {
+    //                 Trie::add_to(next_node, remaining_values)
+    //             } else {
+    //                 let mut node = node.upgrade();
+    //                 let next_node = node.add(cur_value).expect("Internal error: Upgrade occurred but add still not possible");
+    //                 Trie::add_to(next_node, remaining_values);
+    //                 Some(node)
+    //             }
+    //         }
+    //     }
+    // }
 
-    pub fn exists(&self, value: &str) -> bool {
-        let mut cur = &self.index;
+    // pub fn add(&mut self, value: &str) {
+    //     // Trie::add_to(&mut self.root, value.as_bytes())
+    // }
 
-        for c in self.set_case(value).bytes() {
-            match cur.exists(c) {
-                Some(t) => {
-                    cur = &t;
-                }
-                None => return false,
-            }
-        }
+    // pub fn add(&mut self, value: &str) {
+    //     //enum Insert { Full, Found(&dyn Node), NotFound(&dyn Node>) }
+    //     //find -> NotFound Exists
+    //     let mut prev_node :Option<&mut dyn Node> = None;
+    //     let mut cur_node = self.root.as_mut();
+    //     for (i, byte) in value.as_bytes().iter().enumerate() {
+    //         match cur_node.can_add(byte) {
+    //             Location::Exists(index) => {
+    //                 if let Some(child) = cur_node.insert(byte, Location::Exists(index)) {
+    //                     cur_node = child.as_mut();
+    //                 } else {
+    //                     panic!()
+    //                 }
+    //             },
+    //             Location::Insert(index) => {
+    //                 if let Some(child) = cur_node.insert(byte, Location::Exists(index)) {
+    //                     cur_node = child.as_mut();
+    //                 } else {
+    //                     panic!()
+    //                 }
+    //             },
+    //             Location::Upgrade => {
+    //                 if i == 0 {
+    //                     self.root = cur_node.upgrade();
+    //                     cur_node = self.root.as_mut();
+    //                 } else {
+    //                     prev_node = Some(cur_node);
+    //                     cur_node = prev_node.unwrap().upgrade().as_mut();
+    //                 }
+    //                 if let Some(child) = cur_node.add(byte) {
+    //                     cur_node = child.as_mut();
+    //                 } else {
+    //                     panic!("Internal error: Upgrade occurred but add still not possible");
+    //                 }
+    //             },
+    //         }
+    //     }
+    // }
 
-        //look for terminal character if exact match
-        //use is_terminal on last node instead
-        match self.matching {
-            Match::Exact => cur.get_node(256).is_some(), //put in index 0 (\ or null)
-            _ => true,
-        }
-    }
+
+    // pub fn exists(&self, value: &str) -> bool {
+    //     let mut cur = &self.index;
+    //
+    //     for c in self.set_case(value).bytes() {
+    //         match cur.exists(c) {
+    //             Some(t) => {
+    //                 cur = &t;
+    //             }
+    //             None => return false,
+    //         }
+    //     }
+    //
+    //     //look for terminal character if exact match
+    //     //use is_terminal on last node instead
+    //     match self.matching {
+    //         Match::Exact => cur.get_node(256).is_some(), //put in index 0 (\ or null)
+    //         _ => true,
+    //     }
+    // }
 
     fn set_case(&self, value: &str) -> String {
         match self.case {
@@ -57,18 +127,18 @@ impl Trie {
 
 #[cfg(test)] //module should only be compiled for testing
 mod test {
-    use super::{Case, Match, OldNode, Trie};
+    use super::{Case, Match};
 
     //doesnt check terminal char
-    fn only_has_chars(n: &OldNode, s: &str) -> bool {
-        for i in 0_u8..255_u8 {
-            let contain = s.contains(&String::from(i as char));
-            if contain != n.get_node(i as usize).is_some() {
-                return false;
-            }
-        }
-        true
-    }
+    // fn only_has_chars(n: &OldNode, s: &str) -> bool {
+    //     for i in 0_u8..255_u8 {
+    //         let contain = s.contains(&String::from(i as char));
+    //         if contain != n.get_node(i as usize).is_some() {
+    //             return false;
+    //         }
+    //     }
+    //     true
+    // }
 
     #[test]
     fn add_string_chars_exist() {
@@ -113,72 +183,72 @@ mod test {
         //     assert!(only_has_chars(&char_2, "c"));
         // }
     }
-
-    #[test]
-    fn match_empty() {
-        let mut trie = Trie::new(Match::Prefix, Case::Sensitive);
-        trie.add(&"");
-        assert!(trie.exists(&""))
-    }
-
-    #[test]
-    fn match_no_empty() {
-        let trie = Trie::new(Match::Prefix, Case::Sensitive);
-        assert!(trie.exists(&""))
-    }
-
-    #[test]
-    fn match_char() {
-        let mut trie = Trie::new(Match::Prefix, Case::Sensitive);
-        trie.add(&"a");
-
-        assert!(trie.exists(&"a"));
-        assert!(!trie.exists(&"A"));
-    }
-
-    #[test]
-    fn match_string_case_sensitive() {
-        {
-            let mut trie = Trie::new(Match::Exact, Case::Sensitive);
-            trie.add(&"abcde");
-            trie.add(&"abc");
-
-            assert!(trie.exists(&"abcde"));
-            assert!(trie.exists(&"abc"));
-            assert!(!trie.exists(&"ab"));
-            assert!(!trie.exists(&"ABCDE"));
-        }
-
-        {
-            let mut trie = Trie::new(Match::Prefix, Case::Sensitive);
-            trie.add(&"abcde");
-            trie.add(&"abc");
-
-            assert!(trie.exists(&"abcde"));
-            assert!(trie.exists(&"abc"));
-            assert!(trie.exists(&"ab"));
-            assert!(!trie.exists(&"ABCDE"));
-        }
-
-        {
-            let mut trie = Trie::new(Match::PrefixPostfix, Case::Sensitive);
-            trie.add(&"abcde");
-
-            assert!(trie.exists(&"abcde"));
-            assert!(trie.exists(&"abc"));
-            assert!(trie.exists(&"ab"));
-            assert!(trie.exists(&"bcde"));
-            assert!(trie.exists(&"cd"));
-            assert!(!trie.exists(&"ABCDE"));
-        }
-    }
-
-    #[test]
-    fn no_match_string() {
-        let mut trie = Trie::new(Match::Prefix, Case::Sensitive);
-        trie.add(&"abc");
-
-        assert!(!trie.exists(&"bc"));
-        assert!(!trie.exists(&"AB")); //partial complete match
-    }
+    //
+    // #[test]
+    // fn match_empty() {
+    //     let mut trie = Trie::new(Match::Prefix, Case::Sensitive);
+    //     trie.add(&"");
+    //     assert!(trie.exists(&""))
+    // }
+    //
+    // #[test]
+    // fn match_no_empty() {
+    //     let trie = Trie::new(Match::Prefix, Case::Sensitive);
+    //     assert!(trie.exists(&""))
+    // }
+    //
+    // #[test]
+    // fn match_char() {
+    //     let mut trie = Trie::new(Match::Prefix, Case::Sensitive);
+    //     trie.add(&"a");
+    //
+    //     assert!(trie.exists(&"a"));
+    //     assert!(!trie.exists(&"A"));
+    // }
+    //
+    // #[test]
+    // fn match_string_case_sensitive() {
+    //     {
+    //         let mut trie = Trie::new(Match::Exact, Case::Sensitive);
+    //         trie.add(&"abcde");
+    //         trie.add(&"abc");
+    //
+    //         assert!(trie.exists(&"abcde"));
+    //         assert!(trie.exists(&"abc"));
+    //         assert!(!trie.exists(&"ab"));
+    //         assert!(!trie.exists(&"ABCDE"));
+    //     }
+    //
+    //     {
+    //         let mut trie = Trie::new(Match::Prefix, Case::Sensitive);
+    //         trie.add(&"abcde");
+    //         trie.add(&"abc");
+    //
+    //         assert!(trie.exists(&"abcde"));
+    //         assert!(trie.exists(&"abc"));
+    //         assert!(trie.exists(&"ab"));
+    //         assert!(!trie.exists(&"ABCDE"));
+    //     }
+    //
+    //     {
+    //         let mut trie = Trie::new(Match::PrefixPostfix, Case::Sensitive);
+    //         trie.add(&"abcde");
+    //
+    //         assert!(trie.exists(&"abcde"));
+    //         assert!(trie.exists(&"abc"));
+    //         assert!(trie.exists(&"ab"));
+    //         assert!(trie.exists(&"bcde"));
+    //         assert!(trie.exists(&"cd"));
+    //         assert!(!trie.exists(&"ABCDE"));
+    //     }
+    // }
+    //
+    // #[test]
+    // fn no_match_string() {
+    //     let mut trie = Trie::new(Match::Prefix, Case::Sensitive);
+    //     trie.add(&"abc");
+    //
+    //     assert!(!trie.exists(&"bc"));
+    //     assert!(!trie.exists(&"AB")); //partial complete match
+    // }
 }
